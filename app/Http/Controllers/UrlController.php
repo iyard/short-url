3b9adff3f5fd138a06cd46dde941d65df0216842
i-yard <iyardsev@gmail.com>
2020-04-04 12:34:32 +0300

    update UrlController

--- app/Http/Controllers/UrlController.php
+++ app/Http/Controllers/UrlController.php
@@ -1,22 +1,41 @@
 <?php
 
-
 namespace App\Http\Controllers;
 
-
 use App\Url;
+use Illuminate\Http\Request;
 
 class UrlController extends Controller
 {
-
-    /**
-     * Display a listing of the resource.
-     *
-     *
-     */
-    public function index()
+    public function create()
     {
-        $urls = Url::paginate(30);
-        return view('index', compact('urls'));
+        $url = new Url();
+
+        $urls = Url::paginate(20);
+
+        return view('create', compact('url', 'urls'));
+    }
+
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'url' => 'required|url',
+        ]);
+        $url = Url::findOrCreateByUrl($request->url);
+        $url->save();
+
+        $urls = Url::paginate(20);
+
+        return view('create', compact('url', 'urls'));
+    }
+
+    public function redirect($token)
+    {
+        if (!$url = Url::findModelByToken($token)) {
+            return redirect()->route('create');
+        }
+
+        $url->addVisit();
+        return redirect()->away($url->url);
     }
 }
