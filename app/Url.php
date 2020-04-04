5ab186c1c09985c7a46f39e5571bcb403b3f4b36
i-yard <iyardsev@gmail.com>
2020-04-04 12:34:13 +0300

    update Url model

--- app/Url.php
+++ app/Url.php
@@ -21,4 +21,99 @@ class Url extends Model
      * @var string
      */
     protected $table = 'urls';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'url', 'token'
+    ];
+
+    /**
+     * Return unique token
+     *
+     * @return string
+     */
+    public static function getToken()
+    {
+        $token = Token::get();
+        return self::isTokenUnique($token) ? $token : self::getToken();
+    }
+
+    /**
+     * Check token is absent in urls table
+     *
+     * @param $token
+     * @return bool
+     */
+    private static function isTokenUnique($token)
+    {
+        return Url::where('token', $token)
+            ->count() === 0;
+    }
+
+    /**
+     * Check is model already saved in database
+     *
+     * @param string $url
+     * @return bool
+     */
+    private static function isModelCreated(string $url)
+    {
+        return Url::where('url', $url)
+                ->count() > 0;
+    }
+
+    public static function findModelByToken($token)
+    {
+        return Url::where('token', $token)
+            ->first();
+    }
+
+    /**
+     * Find model by url or create new
+     *
+     * @param string $url
+     * @return Url
+     */
+    public static function findOrCreateByUrl(string $url)
+    {
+        return self::isModelCreated($url) ? self::where('url', $url)->first() : new self(['url' => $url]);
+    }
+
+    /**
+     * Boot the model
+     *
+     * @return  void
+     */
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($url) {
+            $url->token = self::getToken();
+        });
+    }
+
+    /**
+     * Return short url
+     *
+     * @param string $baseUrl
+     * @return string
+     */
+    public function getShortUrl(string $baseUrl)
+    {
+        return $baseUrl . DIRECTORY_SEPARATOR . $this->token;
+    }
+
+    /**
+     * Add visit
+     */
+    public function addVisit()
+    {
+        $this->count_visits++;
+        $this->save();
+    }
 }
