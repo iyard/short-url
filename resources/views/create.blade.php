54c16a52a9ff3cba461f932a34609931b730288f
i-yard <iyardsev@gmail.com>
2020-04-04 12:34:56 +0300

    update views

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -5,6 +5,11 @@
 @section('content')
     <h1>Create short url</h1>
 
+    @if(!empty($url->url))
+        <h2>Short url created - <a href="{{ $url->getShortUrl(url("/")) }}">{{ $url->getShortUrl(url("/")) }}</a></h2>
+    @endif
+
+
     @include('shared.form')
 
     <div class="container mt-5">
@@ -14,6 +19,7 @@
             <th scope="col">ID</th>
             <th scope="col">URL</th>
             <th scope="col">TOKEN</th>
+            <th scope="col">SHORT LINK</th>
             <th scope="col">COUNT VISITS</th>
         </tr>
         </thead>
@@ -23,11 +29,15 @@
                 <th scope="row">{{ $url->id }}</th>
                 <td>{{ $url->url }}</td>
                 <td>{{ $url->token }}</td>
+                <td><a href="{{ $url->getShortUrl(url("/")) }}">{{ $url->getShortUrl(url("/")) }}</a></td>
                 <td>{{ $url->count_visits }}</td>
+
             </tr>
         @endforeach
         </tbody>
     </table>
     </div>
+
     {{ $urls->links() }}
+
 @endsection
--- resources/views/shared/form.blade.php
+++ resources/views/shared/form.blade.php
@@ -1,7 +1,10 @@
 {{ Form::model($url, ['url' => route('store')]) }}
     <div class="form-group">
-        {{ Form::label('url', 'Введите адрес') }}
+        {{ Form::label('url', 'Input url') }}
         {{ Form::text('url', $url->url, ['class' => 'form-control']) }}
+        @error('url')
+            <div class="alert alert-danger">{{ $message }}</div>
+        @enderror
     </div>
-    {{ Form::submit('Создать', ['class' => 'btn btn-primary']) }}
+    {{ Form::submit('Create', ['class' => 'btn btn-primary']) }}
 {{ Form::close() }}
